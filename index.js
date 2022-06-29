4c9b0e560715063ac2b29cb12df215fef0e14bbb
sailikpandey22 <102276321+sailikpandey22@users.noreply.github.com>
2022-06-29 21:08:58 +0530

    Uploaded Initial FIles

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".home").hover(
+  function () {
+    $(".static").attr("src", "images/output-onlinegiftools.gif");
+  },
+  function () {
+    $(".static").attr("src", "images/icons8-secured-letter-64.png");
+  }
+);
